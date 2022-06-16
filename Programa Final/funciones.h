eaebd15ea1fa509ad93e5d22cc4f198c75dd651b
alejandromgarces <99010179+alejandromgarces@users.noreply.github.com>
2022-06-16 10:30:20 +0200

    Create funciones.h

---
+++ Programa Final/funciones.h
@@ -0,0 +1,19 @@
+int mov_valido2(struct COORDENADAS Curr,struct COORDENADAS New);
+ int mov_valido(struct COORDENADAS,struct COORDENADAS);
+ int rn_enjake2(struct COORDENADAS);
+ int rb_enjake2(struct COORDENADAS);
+ int rn_enjake();
+ int rb_enjake();
+ int n_total_mov_validos();
+ int b_total_mov_validos();
+ void n_jakemate();
+ void b_jakemate();
+ void n_jake();
+ void b_jake();
+ void n_reyaogado();
+ void b_reyaogado();
+ void n_promocion();
+ void b_promocion();
+void tabla();
+void guardador();
+

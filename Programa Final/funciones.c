b839b4f3c74d92b3493e492b6283278aeb6adf3d
alejandromgarces <99010179+alejandromgarces@users.noreply.github.com>
2022-06-17 15:42:43 +0200

    Update funciones.c

--- Programa Final/funciones.c
+++ Programa Final/funciones.c
@@ -31,51 +31,7 @@ return tablero[8][8];
 
 }
 
-void guardador()
-{
 
- printf ("pulse s para guardar, n para continuar:");
- struct COORDENADAS  pos;
-    char sn;
-
-    FILE* guardado;
-
-    scanf(" %c", sn);
-
-    if( sn=='s')
-   {
-
-      guardado = fopen("partidaguardada.txt", "w");
-
-      if (guardado == NULL)
-      {
-      printf("Error al abrir el fichero.\n");
-      return -1;
-      }
-      else
-      {
-       fprintf(guardado,"   0 1 2 3 4 5 6 7\n");
-       for (pos.fila=0;pos.fila<8;++pos.fila)
-         {
-            fprintf(guardado,"%d |",pos.fila);
-            for(pos.col=0;pos.col<8;++pos.col)
-            fprintf(guardado,"%-2c",tablero[pos.fila][pos.col]);
-            fprintf(guardado,"\n");
-         }
-       fclose(guardado);
-      }
-
-
- }
- else
- {
-     printf("continua");
- }
-
-
-
-
-}
 
 
 int mov_valido2(struct COORDENADAS Curr,struct COORDENADAS New)

d5f8adf7e0413f7510f424e5a760cca254b08817
alejandromgarces <99010179+alejandromgarces@users.noreply.github.com>
2022-06-18 15:25:16 +0200

    Update funciones.c

--- Programa Final/funciones.c
+++ Programa Final/funciones.c
@@ -1611,7 +1611,46 @@ return tablero[8][8];
 
 }
 
+void guardar()
+{
 
+
+  FILE *guardado;
+
+  char tablero[8][8];
+
+  struct COORDENADAS pos;
+
+
+
+
+
+  guardado = fopen("guardado.txt", "w");
+
+
+  if (guardado == NULL)
+      {
+      printf("Error al abrir el fichero.\n");
+      return -1;
+      }
+  else
+      {
+
+
+    fprintf(guardado,"   0 1 2 3 4 5 6 7\n");
+       for (pos.fila=0;pos.fila<8;++pos.fila)
+         {
+            fprintf(guardado,"%d |",pos.fila);
+            for(pos.col=0;pos.col<8;++pos.col)
+            fprintf(guardado,"%-2c",tablero[pos.fila][pos.col]);
+            fprintf(guardado,"\n");
+         }
+       fclose(guardado);
+        }
+
+
+return 0;
+}
 
 
 int mov_valido2(struct COORDENADAS Curr,struct COORDENADAS New)
